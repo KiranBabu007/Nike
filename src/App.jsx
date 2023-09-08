7ca21aad2aa27fcce0c19eecbda6cccb75ed8fac
Kiran Babu <kiranbabu3355@gmail.com>
2023-09-09 01:13:56 +0530

    Sections added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-export default function App() {
-    return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
-}
+const App = () =>(
+<main classname="relative">
+    Nav
+    <section classname="xl:padding-1
+    wide :padding-r padding-b">
+        Hero
+    </section>
+    <section className="padding">
+        PopularProducts
+    </section>
+    <section className="padding">
+        SuperQuality
+    </section>
+    <section className="padding-x py-10">
+        Services
+    </section>
+    <section className="padding">
+        SpecialOffer
+    </section>
+    <section className="bg-pale-blue padding">
+        CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16">
+        Subscribe
+    </section>
+    <section className="padding-x padding-t pb-8 bg-black">
+        Footer
+    </section>
+    
+</main>
+)
+
+
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,17 @@ export default {
       '8xl': ['96px', '106px']
     },
     extend: {
+      screens: {
+        xs: ['12px', '16px'],
+        sm: ['14px', '20px'],
+        base: ['16px', '19.5px'],
+        lg: ['18px', '21.94px'],
+        xl: ['20px', '24.38px'],
+        '2xl': ['24px', '29.26px'],
+        '3xl': ['28px', '50px'],
+        '4xl': ['48px', '58px'],
+        '8xl': ['96px', '106px']
+      },
       fontFamily: {
         palanquin: ['Palanquin', 'sans-serif'],
         montserrat: ['Montserrat', 'sans-serif'],
@@ -40,5 +51,14 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    
+  ],
+  corePlugins: {
+    // ...
+    '@layer base': [],
+    '@layer components': ['sm'], // Include the sm: class here
+    '@layer utilities': [],
+  },
 }
+
